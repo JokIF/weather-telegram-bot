ca71e93dbdae26567bba86ccb0a33c0d6f6943d1
JokIF <jokifoki2@gmail.com>
2023-06-07 21:41:18 +0300

    docker debug

--- main_bot/utils/redis_static.py
+++ main_bot/utils/redis_static.py
@@ -4,7 +4,7 @@ from main_bot import config
 
 redis_main = Redis(
     host=config.REDIS_HOST,
-    port=config.REDIS_PORT)
-    # db=config.REDIS_DB,
-    # password=config.REDIS_PASSWORD)
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    password=config.REDIS_PASSWORD)
 redis_main.build_key = lambda chat_id, user_id: ":".join(("fsm", str(chat_id), str(user_id), "static"))
